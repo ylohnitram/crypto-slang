8ff90a8ea7dd13cb0bee6b5fcfcb7bc6e881bada
Martin Holy <mholy1983@protonmail.com>
2025-03-01 01:43:47 +0100

    fix(sitemap generator): wrong base url

--- generate-sitemap.js
+++ generate-sitemap.js
@@ -1,26 +1,27 @@
 const fs = require('fs');
 const terms = require('./data/terms.json');
 
-const baseUrl = 'https://coin-exchange.cz/#/term/';
+// !!! ZMĚŇTE NA SVOJI URL !!!
+const BASE_URL = 'https://ylohnitram.github.io/crypto-slang/'; 
 
-const termUrls = terms.map(term => `
+const urls = terms.map(term => `
   <url>
-    <loc>${baseUrl}${term.slug}</loc>
+    <loc>${BASE_URL}#/term/${term.slug}</loc>
     <lastmod>${new Date().toISOString()}</lastmod>
-    <changefreq>weekly</changefreq>
+    <changefreq>monthly</changefreq>
   </url>
 `).join('');
 
 const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
   <url>
-    <loc>https://coin-exchange.cz/</loc>
+    <loc>${BASE_URL}</loc>
     <lastmod>${new Date().toISOString()}</lastmod>
     <changefreq>daily</changefreq>
     <priority>1.0</priority>
   </url>
-  ${termUrls}
+  ${urls}
 </urlset>`;
 
-fs.writeFileSync('./public/sitemap.xml', sitemap);
-console.log('Sitemap generated!');
+fs.writeFileSync('./sitemap.xml', sitemap);
+console.log('✅ Sitemap generated!');
